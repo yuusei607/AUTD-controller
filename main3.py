4799ddca3acf6df73a6b79c48df3bcd802c9cf17
yusei <yuusei1467@g.ecc.u-tokyo.ac.jp>
2025-11-04 17:18:26 +0900

    a

--- main3.py
+++ main3.py
@@ -9,8 +9,8 @@ from pyautd3 import (
     Sine,
     SineOption,
 )
-from pyautd3_link_ethercrab import EtherCrab, EtherCrabOption, Status
-
+from pyautd3.link.ethercrab import EtherCrab, EtherCrabOption, Status
+from pyautd3.modulation import Fourier, FourierOption
 
 
 def err_handler(idx: int, status: Status) -> None:
@@ -18,6 +18,7 @@ def err_handler(idx: int, status: Status) -> None:
 
 
 if __name__ == "__main__":
+    start_time = time.time()
     with Controller.open(
         [AUTD3(pos=[0.0, 0.0, 0.0], rot=[1, 0, 0, 0])],
         EtherCrab(err_handler=err_handler, option=EtherCrabOption()),
@@ -31,13 +32,30 @@ if __name__ == "__main__":
 
         autd.send(Silencer())
 
-        g = Focus(
-            pos=autd.center() + np.array([0.0, 0.0, 150.0]),
-            option=FocusOption(),
-        )
-        m = Sine(
-            freq=150 * Hz,
-            option=SineOption(),
+        center = np.array([0.0, 0.0, 150.0])
+        point_num = 200
+        radius = 30.0
+
+        g = FociSTM(
+                foci=(
+                    center + radius * np.array([np.cos(theta), np.sin(theta), 0])
+                    for theta in (2.0 * np.pi * i / point_num for i in range(point_num))
+                ),
+                config=1.0 * Hz,
+            )
+
+        
+        m = Fourier(
+            components=[
+                Sine(freq=30 * Hz, option=SineOption(intensity=a)),
+                Sine(freq=200 * Hz, option=SineOption(intensity=b)),
+                Static(intensity=c),
+            ], # a + b + c = 255(0xff)
+            option=FourierOption(
+                scale_factor=None,
+                clamp=False,
+                offset=0x00,
+            ),
         )
         autd.send((m, g))
 
